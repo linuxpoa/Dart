a573084a5e65c00041cb79ff099eb61e5b407d51
Marco Azevedo <mitce.cadastro@gmail.com>
2023-10-25 17:04:48 -0300

    Add files via upload

---
+++ Aulas/aula_01.dart
@@ -0,0 +1,50 @@
+//Função (main) e saída (output)
+
+/* main(){
+  print("Hello World");
+  print("segunda impressão");
+  print("terceira impressão");
+  print(3*5);
+  print(3.14);
+}
+*/
+
+//variáveis
+/*
+main()
+
+var hello = "Hello world";
+hello = "Outro valor";
+
+print(hello);
+
+var numero = 2;
+numero = 3;
+
+print(numero);
+*/
+
+/* Operações Matemáticas*/
+
+/* Resto de divisão 
+main(){
+
+  var num1 = 10;
+  var num2 = 2;
+
+  var total = num1 % num2;
+  
+  print (total);
+}
+*/
+/* Concatenação de textos 
+main(){
+
+  var text1 = "Marco";
+  var text2 = "Azevedo";
+
+  var total = text1 + text2;
+
+  print(total);
+
+}
